7f75652fc6ef79a4bbc49d67dbe6a965122ab8b9
smoothlyraining <tomas.sarmiento@gmail.com>
2020-03-02 10:51:43 -0300

    Listo

---
+++ JavaFun.java
@@ -0,0 +1,7 @@
+public class JavaFun{
+    public static void main(String[] args){
+        System.out.println("Mi nombre es Tomás Sarmiento");
+        System.out.println("Tengo 19 años");
+        System.out.println("Vivo en Temuco, Chile");
+    }
+}
